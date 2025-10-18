877e20a76d4e0459fe8d4094989b3bf87782c711
LEUREON <leureon@gmail.com>
2025-10-19 00:52:26 +0300

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,24 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import App from "./App"
-import "./index.css"
-import { lockLargeViewportHeight } from "./utils/lockLVH"
-lockLargeViewportHeight()
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { lockLargeViewportHeight } from './utils/lockLVH';
+lockLargeViewportHeight();
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+// Регистрируем наш sw.js
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/sw.js') // Регистрируем sw.js из папки public
+    .then((registration) => {
+      console.log('[SW] Service Worker УСПЕШНО зарегистрирован:', registration);
+    })
+    .catch((error) => {
+      console.error('[SW] ОШИБКА регистрации Service Worker:', error);
+    });
+}
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-)
+  </React.StrictMode>,
+);
